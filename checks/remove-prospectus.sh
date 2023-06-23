4d627f2ec5b8e1bede7149bbb85116fd4526c2c9
Les Aker <me@lesaker.org>
2023-06-22 21:13:09 -0400

    initial commit

---
+++ checks/remove-prospectus.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+rm -rf .prospectus
+
+echo -n '{"name":"Remove .prospectus files","branch":"cultivator-prospectus","body":"We no longer use prospectus files","commit_msg":"remove .prospectus"}'
+
