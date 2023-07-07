f39119e0adf17fd651a1de0f63d7941210378884
Les Aker <me@lesaker.org>
2023-07-06 22:39:51 -0400

    now more quiet

--- checks/submodule.sh
+++ checks/submodule.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git submodule foreach 'git submodule update --init --recursive --remote'
+git submodule foreach --quiet 'git submodule update --quiet --init --recursive --remote'
 
 echo -n '{"name":"Update submodules","branch":"cultivator/submodules","body":"Keep git submodules up to date","commit_msg":"update submodules"}'
 
