e470a8e0049313ceb0e0d2ce8feb42ac62c52dbe
Les Aker <me@lesaker.org>
2023-07-07 10:39:44 -0400

    add rubygems check

---
+++ checks/rubygems.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export ORG=akerl
+export NAME=cultivator-rubygems
+export VERSION=v0.0.1
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+"$SCRIPT_DIR/../scripts/download_and_run.sh" "$1"
