ba49b15ecb18ae66829c3352901085082933e4ea
Les Aker <me@lesaker.org>
2023-07-06 22:35:26 -0400

    smarter submodule update

--- checks/submodule.sh
+++ checks/submodule.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git submodule foreach 'git pull && git submodule update --init --recursive'
+git submodule foreach 'git submodule update --init --recursive --remote'
 
 echo -n '{"name":"Update submodules","branch":"cultivator/submodules","body":"Keep git submodules up to date","commit_msg":"update submodules"}'
 
